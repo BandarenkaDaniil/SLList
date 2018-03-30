bab24490ad9510795b38d98e8a7cb427fb49749b
Daniil Bondarenko <danikbrest@icloud.com>
2018-03-30 21:48:30 +0300

    add source files

---
+++ src/SLList.c
@@ -0,0 +1,75 @@
+#include "SLList.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void sll_init(struct SLList* source_list)
+{
+    source_list->head        = (struct Node*)malloc(sizeof(struct Node*));
+    source_list->head->value = NULL;
+    source_list->head->next  = NULL;
+
+    source_list->size = 0;
+}
+
+void sll_terminate(struct SLList* source_list)
+{
+    while (source_list->head->next != NULL)
+    {
+    	struct Node *temp = source_list->head->next;
+
+    	free(source_list->head);
+    	source_list->head = temp;
+    }
+}
+
+void sll_push(struct SLList* source_list, char* new_string)
+{
+    struct Node *new_node = (struct Node*)malloc(sizeof(struct Node*));
+
+    new_node->value = (char*)malloc(sizeof(char*) * (strlen(new_string) + 1));
+    strcpy(new_node->value, new_string);
+    new_node->next = NULL;
+
+    struct Node *temp = source_list->head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new_node;
+
+    source_list->size++;
+}
+
+char* sll_pop(struct SLList* source_list/*, char** dest*/)
+{
+    struct Node *temp = source_list->head;
+
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+	char *result =  (char*)malloc(sizeof(char*) * (strlen(temp->next->value) + 1));
+	strcpy(result, temp->next->value);
+
+    free(temp->next);
+
+    temp->next = NULL;
+
+    source_list->size--;
+
+	return result;
+}
+
+bool sll_is_empty(struct SLList* source_list)
+{
+    return (source_list->size == 0);
+}
+
+int sll_size(struct SLList* source_list)
+{
+	return source_list->size;
+}
---
+++ src/SLList.h
@@ -0,0 +1,27 @@
+#ifndef SLLIST_H
+#define SLLIST_H
+
+#include <stdbool.h>
+
+struct Node
+{
+    char *value;
+    struct Node *next;
+};
+
+struct SLList
+{
+    struct Node *head;
+    int size;
+};
+
+void sll_init      (struct SLList*);
+void sll_terminate (struct SLList*);
+
+void  sll_push (struct SLList*, char*);
+char* sll_pop  (struct SLList*);
+
+bool sll_is_empty (struct SLList*);
+int  sll_size     (struct SLList*);
+
+#endif // SLLIST_H
