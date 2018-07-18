ab9c01be8f3874c5328633a43c0440f193b1aaf6
Daniil Bondarenko <danikbrest@icloud.com>
2018-07-18 17:22:45 +0300

    change sll_empty function's return value type

--- src/SLList.h
+++ src/SLList.h
@@ -1,27 +1,25 @@
 #ifndef SLLIST_H
 #define SLLIST_H
 
-#include <stdbool.h>
-
 struct Node
 {
-    char *value;
-    struct Node *next;
+	char *value;
+	struct Node *next;
 };
 
 struct SLList
 {
-    struct Node *head;
-    int size;
+	struct Node *head;
+	int size;
 };
 
-void sll_init      (struct SLList*);
-void sll_terminate (struct SLList*);
+void sll_init(struct SLList*);
+void sll_terminate(struct SLList*);
 
-void  sll_push (struct SLList*, char*);
-void  sll_pop  (struct SLList*);
+void  sll_push(struct SLList*, char*);
+char* sll_pop (struct SLList*);
 
-bool sll_is_empty (struct SLList*);
-int  sll_size     (struct SLList*);
+int sll_is_empty(struct SLList*);
+int sll_size(struct SLList*);
 
 #endif // SLLIST_H
