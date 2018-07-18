74533693c10547fea36476727de8788d6786d3e5
Daniil Bondarenko <danikbrest@icloud.com>
2018-07-18 17:18:10 +0300

    fix memory leaking bug

--- src/SLList.c
+++ src/SLList.c
@@ -1,67 +1,78 @@
 #include "SLList.h"
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void sll_init(struct SLList* source_list)
 {
-    source_list->head        = (struct Node*)malloc(sizeof(struct Node*));
-    source_list->head->value = NULL;
-    source_list->head->next  = NULL;
+	source_list->head = malloc(sizeof(struct Node));
+	source_list->head->value = NULL;
+	source_list->head->next = NULL;
 
-    source_list->size = 0;
+	source_list->size = 0;
 }
 
 void sll_terminate(struct SLList* source_list)
 {
-    while (source_list->head->next != NULL)
-    {
-    	struct Node *temp = source_list->head->next;
+	while (source_list->size)
+	{
+		free(sll_pop(source_list));
+	}
 
-    	free(source_list->head);
-    	source_list->head = temp;
-    }
+	free(source_list->head);
 }
 
 void sll_push(struct SLList* source_list, char* new_string)
 {
-    struct Node *new_node = (struct Node*)malloc(sizeof(struct Node*));
+	struct Node *new_node = malloc(sizeof(struct Node));
+	
+	new_node->value = malloc(sizeof(char) * (strlen(new_string) + 1));
+	strcpy(new_node->value, new_string);
+	new_node->next = NULL;
 
-    new_node->value = (char*)malloc(sizeof(char*) * (strlen(new_string) + 1));
-    strcpy(new_node->value, new_string);
-    new_node->next = NULL;
+	struct Node *temp = source_list->head;
 
-    struct Node *temp = source_list->head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new_node;
 
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = new_node;
-
-    source_list->size++;
+	source_list->size++;
 }
 
-void sll_pop(struct SLList* source_list)
+char* sll_pop(struct SLList* source_list)
 {
-    struct Node *temp = source_list->head;
+	if (source_list->size)
+	{
+		struct Node *temp = source_list->head;
 
-    while (temp->next->next != NULL)
-    {
-        temp = temp->next;
-    }
+		while (temp->next->next != NULL)
+		{
+			temp = temp->next;
+		}
 
-    free(temp->next);
+		char *result = (char*)malloc(sizeof(char) * (strlen(temp->next->value) + 1));
+		strcpy(result, temp->next->value);
 
-    temp->next = NULL;
+		free(temp->next->value);
+		free(temp->next);
 
-    source_list->size--;
+		temp->next = NULL;
+
+		source_list->size--;
+
+		return result;
+	}
+	else
+	{
+		return NULL;
+	}
 }
 
-bool sll_is_empty(struct SLList* source_list)
+int sll_is_empty(struct SLList* source_list)
 {
-    return (source_list->size == 0);
+	return (source_list->size == 0);
 }
 
 int sll_size(struct SLList* source_list)
